d8d6d1c0a245c1670d92e5fb82c95678d2945b6a
nunomiguel1995 <nuno.migcastro@gmail.com>
2016-04-13 10:15:27 +0100

    Separação em funções

--- listdir.c
+++ listdir.c
@@ -8,28 +8,23 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-int main(int argc, char *argv[])
-{
+int read_directory(char *dir_name){
+
 	DIR *dirp;
 	struct dirent *direntp;
 	struct stat stat_buf;
 	char *str;
 	char name[200];
-	if (argc != 2){
-		fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
-		exit(1);
-	}
-	if ((dirp = opendir( argv[1])) == NULL){
-		perror(argv[1]);
-		exit(2);
+
+	if ((dirp = opendir( dir_name)) == NULL){
+		return 1;
 	}
 
 	while ((direntp = readdir( dirp)) != NULL){
 		if(direntp->d_name[0] != '.'){ //nao lista ficheiros ocultos
-			sprintf(name,"%s/%s",argv[1],direntp->d_name);
+			sprintf(name,"%s/%s",dir_name,direntp->d_name);
 			if (lstat(name, &stat_buf)==-1)	{
-				perror("lstat ERROR");
-				exit(3);
+				return 2;
 			}
 			if (S_ISREG(stat_buf.st_mode)) str = "regular";
 			else if (S_ISDIR(stat_buf.st_mode)) str = "directory";
@@ -38,5 +33,24 @@ int main(int argc, char *argv[])
 		}
 	}
 	closedir(dirp);
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	if (argc != 2){
+		fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
+		exit(1);
+	}
+
+	int read_dir = read_directory(argv[1]);
+
+	if(read_dir == 1){
+		perror(argv[1]);
+		exit(2);
+	}else if(read_dir == 2){
+		perror("lstat ERROR");
+		exit(3);
+	}
+
 	exit(0);
 }
