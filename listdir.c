7890c5e1b445a5375777be507a43d9576356cfb8
nunomiguel1995 <nuno.migcastro@gmail.com>
2016-04-14 12:48:06 +0100

    Já escreve no ficheiro o nome dos ficheiros

--- listdir.c
+++ listdir.c
@@ -13,41 +13,36 @@
 #include <fcntl.h>
 #include "utilities.h"
 
-int read_directory(char *dir_name){
+int read_directory(char *dir_name, FILE *file){
 	DIR *dirp;
 	struct dirent *direntp;
 	struct stat stat_buf;
 	char *str;
 	char name[200];
-	pid_t pid;
 	char *path = strcat(dir_name,"/");
-	int file;
+	pid_t pid;
 
 	if ((dirp = opendir( dir_name)) == NULL){
 		return 1;
 	}
 
-	file= open (FILE_NAME, O_WRONLY | O_CREAT , 0644);
-	if(file == -1){
-		return 2;
-	}
-
 	while ((direntp = readdir( dirp)) != NULL){
-		if(direntp->d_name[0] != '.'){ //nao lista ficheiros ocultos
+		if(direntp->d_name[0] != '.'){ /*nao lista ficheiros ocultos*/
 			sprintf(name,"%s/%s",dir_name,direntp->d_name);
 			if (lstat(name, &stat_buf)==-1)	{
-				return 3;
+				return 2;
 			}
 			if (S_ISREG(stat_buf.st_mode)){
 				str = "regular";
-				char *file_name = direntp->d_name;
-				write(file,str,sizeof(str));
+				char f_name[50];
+				strcpy(f_name,direntp->d_name);
+				fprintf(file,"%s\n",f_name);
 			}
 			else if (S_ISDIR(stat_buf.st_mode)){
 				str = "directory";
 				pid = fork();
 				if(pid == 0){ /*filho*/
-					read_directory(strcat(path,direntp->d_name));
+					read_directory(strcat(path,direntp->d_name),file);
 					return 0;
 				}else{ /*pai*/
 					waitpid(pid,NULL,0);
@@ -57,28 +52,36 @@ int read_directory(char *dir_name){
 			printf("%-25s - %s\n", direntp->d_name, str);
 		}
 	}
+
 	closedir(dirp);
-	close(file);
 	return 0;
 }
 
 int main(int argc, char *argv[]){
+	FILE *file;
+	int read_dir;
+
 	if (argc != 2){
-		fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
+		fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
 		exit(1);
 	}
 
-	int read_dir = read_directory(argv[1]);
+	file = fopen(FILE_NAME,"w");
+	if(file == NULL){
+		perror("file ERROR");
+		exit(1);
+	}
+
+	read_dir = read_directory(argv[1],file);
 
 	if(read_dir == 1){
 		perror(argv[1]);
 		exit(2);
-	}else if(read_dir == 3){
+	}else if(read_dir == 2){
 		perror("lstat ERROR");
 		exit(3);
-	}else if(read_dir == 2){
-		exit(4);
 	}
 
+	fclose(file);
 	exit(0);
 }
