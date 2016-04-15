4afb11e1e4edb2b6f301c2ccf9c2c98f2404bca3
nunomiguel1995 <nuno.migcastro@gmail.com>
2016-04-15 12:15:39 +0100

    Path dinâmico

--- rmdup.c
+++ rmdup.c
@@ -4,15 +4,39 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[], char *envp[]){
 
-  if(argc != 2){
-    printf("Usage: %s <dir>\n", argv[0]);
-    return 1;
-  }
+	if(argc != 2){
+		printf("Usage: %s <dir>\n", argv[0]);
+		return 1;
+	}
 
-  execlp("/home/nuno/workspace/proj_sope/listdir","listdir",argv[1],NULL);
+	char *buf;
+	char *listdir;
+	char *dir;
+	long size;
 
-  return 0;
+	size = pathconf(".", _PC_PATH_MAX);
+
+	if((buf = (char *)malloc((size_t) size)) != NULL){
+		listdir= getcwd(buf, (size_t)size);
+	}
+
+	if(listdir == NULL){
+		printf("Error getting current working path\n");
+		return 1;
+	}
+
+	dir= getenv("HOME");
+
+	strcat(listdir,"/");
+	strcat(listdir,"listdir");
+	strcat(dir,"/");
+	strcat(dir,argv[1]);
+
+	execlp(listdir,"listdir",dir,NULL);
+
+	return 0;
 }
